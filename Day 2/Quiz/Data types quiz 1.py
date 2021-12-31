9fba37d634a27feb70f04d5421ff31dac53d073e
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-12-31 12:46:49 +0530

    Create Data types quiz 1.py

---
+++ Day 2/Quiz/Data types quiz 1.py
@@ -0,0 +1,18 @@
+Question 1:
+Which statement is incorrect?
+=>"False" is a boolean
+
+Question 2:
+What is the data type of the mystery variable?
+
+mystery = 734_529.678
+=>Float
+
+Question 3:
+I've put a spell on you. You are now a computer. If I give you the following code, what will you print out?
+
+street_name = "Abbey Road"
+print(street_name[4] + street_name[7])
+
+mystery = 734_529.678
+=>yo

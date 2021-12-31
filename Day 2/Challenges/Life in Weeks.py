bdf868786807785d4d5fde00dbe91344acd8deb4
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-12-31 21:18:46 +0530

    Create Life in Weeks.py

---
+++ Day 2/Challenges/Life in Weeks.py
@@ -0,0 +1,5 @@
+age = 90-int(input("What is your current age?"))
+days=age*365
+weeks=age*52
+month=12*age
+print("You have "+str(days)+" days, "+str(weeks)+" weeks, "+"and "+str(month)+" months left.")

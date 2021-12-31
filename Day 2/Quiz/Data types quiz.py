6926d0a04cbc52433e83a4f2f014407b72f03f18
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-12-31 12:45:45 +0530

    Create Data types quiz.py

---
+++ Day 2/Quiz/Data types quiz.py
@@ -0,0 +1,15 @@
+Question 1:
+Which statement is incorrect?
+=>"False" is a boolean
+
+Question 2:
+What is the data type of the mystery variable?
+
+mystery = 734_529.678
+=>Float
+
+Question 2:
+What is the data type of the mystery variable?
+
+mystery = 734_529.678
+=>yo

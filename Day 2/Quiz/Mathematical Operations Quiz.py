465047a5ec8f56ca5807df61a299103c955ec157
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2022-01-02 19:38:14 +0530

    Create Mathematical Operations Quiz.py

---
+++ Day 2/Quiz/Mathematical Operations Quiz.py
@@ -0,0 +1,16 @@
+Question 1:
+You are a computer. What will this line of code print?
+
+print(6 + 4 / 2 - (1 * 2))
+=>6.0
+
+Question 2:
+What is the data type of the result of the variable a in the following line of code:
+
+a = int("5") / int(2.7)
+=>float
+
+Question 3:
+Which of these lines of code will give you an error?
+=>age=12
+print("your age"+age+"yeras old")

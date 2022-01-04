f9f87517b5a7bb63f833548f8b9551a7bad59923
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2022-01-04 12:35:37 +0530

    Create Random module.py

---
+++ Day 4/Practice/Random module.py
@@ -0,0 +1,7 @@
+import random
+random_integer=random.randint(1,10)
+print(random_integer)
+radom_float=random.random()
+print(radom_float*5)
+love_score=random.randint(1,100)
+print(f"your love score is {love_score}")

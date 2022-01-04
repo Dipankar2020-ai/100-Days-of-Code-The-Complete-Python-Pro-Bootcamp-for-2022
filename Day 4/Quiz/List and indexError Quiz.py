aaf19d4f3f7a71621226f563338c7efd9b71cd1e
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2022-01-04 20:31:31 +0530

    Create List and indexError Quiz.py

---
+++ Day 4/Quiz/List and indexError Quiz.py
@@ -0,0 +1,29 @@
+Question 1:
+Given the following list:
+
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+Which line of code will give you "Apples"?
+=>fruits[-5]
+
+Question 2:
+Given the code below:
+
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+fruits[-1] = "Melons"
+fruits.append("Lemons")
+print(fruits)
+What do you think will be printed?
+=>["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Melons", "Lemons"]
+
+Question 3:
+Given the code below:
+
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+ 
+dirty_dozen = [fruits, vegetables]
+ 
+print(dirty_dozen[1][1])
+What will be printed?
+=>"Kale"
+

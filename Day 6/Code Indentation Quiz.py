e10e43771470d0d3a100a8df2d93c288d3897b50
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2022-01-07 08:58:34 +0530

    Create Code Indentation Quiz.py

---
+++ Day 6/Code Indentation Quiz.py
@@ -0,0 +1,22 @@
+Question 1:
+Which version of code will produce an Indentation Error when it is run?
+
+NOTE: The • symbol represents a single space.
+=>def my_function():
+  print("Hello")
+
+Question 2:
+Which version of code will output "This will run"?
+=>def my_function():
+      #This is my function
+      print("This will run")
+  my_function()
+
+Question 3:
+In which version of code, will you see "This will run" printed?
+=>def my_function():
+      a=3
+      if a>2:
+           print("Hello World")
+   my_function()
+ 
